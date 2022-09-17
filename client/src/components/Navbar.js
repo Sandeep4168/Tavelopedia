db3001c41c6ffbd77d867f5401554ecd672c7eaa
Sandeep Yadawad <sandu4168@gmail.com>
2022-09-18 01:11:22 +0530

    aNavbar and Login

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -8,18 +8,18 @@ const Navbar = () => {
 
   const handleClose = () => setNav(!nav)
   return (
-    <div  className="w-screen h-[80px] z-10 fixed opacity-100 ">
+    <div  className="w-screen h-[80px] fixed z-10 opacity-100 ">
       <div className="px-2 flex justify-between items-center w-full h-full ">
         <div className="flex items-center">
-          <h1 className="text-3xl font-bold text-black font-OpenSans m-4 px-8 sm:text-3xl"> Travelopedia.</h1>
+          <h1 className="text-3xl font-bold text-white font-OpenSans m-4 px-8 sm:text-3xl"> Travelopedia.</h1>
         
         </div>
         <div className="hidden md:flex pr-4">
-          <button className="border-none bg-transparent text-black mr-4 font-OpenSans">
+          <button className="border-none  bg-transparent hover:scale-110  text-white mr-4 font-OpenSans">
             
             Sign In
           </button>
-          <button className="px-8 rounded-full bg-black py-3 font-OpenSans border-none"> Sign up</button>
+          <button className="px-8 rounded-full bg-white text-black hover:text-white   py-3 font-OpenSans border-none"> Sign up</button>
         </div>
         <div className="md:hidden mr-4" onClick={handleClick}>
           {!nav ? <Bars3Icon className="w-5" /> : <XMarkIcon className="w-5" />}
--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,9 +1,58 @@
-import React from 'react'
+import React,{useState,useEffect} from 'react'
+import {Link} from "react-router-dom"
+import bgImg from "../images/bgImg.jpg"
 
-const login = () => {
+
+const initialState = {
+  email:"",
+  password:""
+}
+
+
+
+
+const Login = () => {
+  
+  const [formValue,setFormValue] = useState(initialState)
+  const {email, password} = formValue
+  
+  
   return (
-    <div>login</div>
+    
+    <div className="w-screen h-auto  ">
+     
+       {/* <img className=" w-screen absolute" src={bgImg} alt="/"/>  */}
+      
+        
+    <div className="absolute justify-center bg-white" >
+        
+<form className=" absolute w-[400px] mx-[400px] my-[200px] font-OpenSans bg-[url('/images/bgImg.jpg')]" >
+  <div class="mb-6">
+    <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Your email</label>
+    <input type="email" id="email" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5  dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="username" required="true"/>
+  </div>
+  <div class="mb-6">
+    <label for="password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">password</label>
+    <input type="password" id="password" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5  dark:border-gray-600  dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="password" required=""/>
+  </div>
+  <div class="flex items-start mb-6">
+    <div class="flex items-center h-5">
+      <input id="remember" type="checkbox" value="" class="w-4 h-4 bg-gray-50 rounded border border-gray-300 focus:ring-3 focus:ring-blue-300  dark:border-gray-600 dark:focus:ring-blue-600 dark:ring-offset-gray-800" required=""/>
+    </div>
+    <label for="remember" class="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">Remember me</label>
+  </div>
+  <button className="px-8 rounded-full bg-white text-black hover:text-white   py-3 font-OpenSans border-none"> Submit</button>
+</form>
+
+         
+      
+     
+</div>
+      
+      
+      
+    </div>
   )
 }
 
-export default login
+export default Login
