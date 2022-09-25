4d304d5f835f5fc723bff289fcfbcca42a78d65e
Sandeep Yadawad <sandu4168@gmail.com>
2022-09-25 23:26:08 +0530

    Modification of dashboardcard

--- client/src/components/DashboardCard.js
+++ client/src/components/DashboardCard.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
-import { FaRegTrashAlt } from "react-icons/fa";
+import { FaRegTrashAlt, FaPencilAlt } from "react-icons/fa";
 import { useNavigate } from "react-router-dom";
 const CardTour = ({
   imageFile,
@@ -22,54 +22,78 @@ const CardTour = ({
   };
 
   const handleDelete = () => {
-
-    navigate("/")
-
-  }
+    navigate("/");
+  };
 
   return (
-    <Link to={`/tour/${_id}`}>
-      <div>
+    <div>
+      <div className="flex flex-col max-w-[400px]">
+        <Link to={`/tour/${_id}`}>
+          <div
+            onMouseEnter={handleText}
+            onMouseLeave={handleRemoveText}
+            className="relative rounded-xl h-[300px]"
+          >
+            <img
+              className="w-full h-full object-cover rounded-xl"
+              src={imageFile}
+              alt="/"
+            />
+            <div
+              className={
+                text
+                  ? " bg-white opacity-80  duration-500  absolute top-0 left-0 w-full h-full rounded-xl"
+                  : " duration-500 bg-gray-900/30 absolute top-0 left-0 w-full h-full rounded-xl"
+              }
+            >
+              <p
+                className={
+                  text
+                    ? "absolute text-2xl opacity-1 w-full h-full lg:text-xl  p-4 "
+                    : "absolute text-2xl opacity-0 w-full h-full lg:text-xl p-4 "
+                }
+              >
+                {" "}
+                {description}
+              </p>
+              <p className="left-4 bottom-4 text-2xl font-bold text-white absolute">
+                {title}
+              </p>
+            </div>
+          </div>
+        </Link>
         <div
           onMouseEnter={handleText}
           onMouseLeave={handleRemoveText}
-          className="relative rounded-xl h-[300px]"
+          className=" flex justify-end  mt-[-44px]  bg-transparent z-10"
         >
-          <img
-            className="w-full h-full object-cover rounded-xl"
-            src={imageFile}
-            alt="/"
-          />
-          <div className="bg-gray-900/30  duration-500 hover:bg-white hover:opacity-80  absolute top-0 left-0 w-full h-full rounded-xl">
-            <p
-              className={
-                text
-                  ? "absolute text-2xl opacity-1 z-10 p-4 "
-                  : "absolute text-xl opacity-0 z-10 p-4 "
-              }
-            >
-              {" "}
-              {description}
-            </p>
-            <p className="left-4 bottom-4 text-2xl font-bold text-white absolute">
-              {title}
-            </p>
-            <div
-        onClick={handleDelete}
-        className=" bottom-4 right-3 hover:scale-105  absolute bg-transparent z-20"
-      >
-        <FaRegTrashAlt
-          className={text ? " text-black" : " text-white "}
-          size={30}
+            <div className=" flex z-20 px-2">
+                <div className={text ? "hover:scale-105 text-indigo-800 flex px-2 cursor-pointer " : "hover:scale-105 text-white flex px-2 cursor-pointer"}>
+                <FaPencilAlt
+           onClick={handleDelete}
+          
+          size={25}
         />
-      </div>
-          </div>
-        </div>
-        
-      </div>
+                </div>
+                <div className={
+              text
+                ? "hover:scale-105 text-red-800 flex cursor-pointer"
+                : "hover:scale-105 text-white flex cursor-pointer"
+            }>
+                <FaRegTrashAlt
+            onClick={handleDelete}
+            
+            size={25}
+          />
 
-      
-    </Link>
+                </div>
+            
+          
+          
+        </div>
+        </div>
+      </div>
+    </div>
   );
 };
 
